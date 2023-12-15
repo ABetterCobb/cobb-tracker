ccbde631255d21ff27d07a7047032ccd4e74bbf4
Guyliner <guyliner69@proton.me>
2023-12-15 03:04:26 -0500

    Tetsing

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
-response = requests.get('https://www.mariettaga.gov/AgendaCenter')
+
+headers = {'User-Agent': 'Mozilla/5.0'}
+values = {"year": "2015", "catID": "7"}
+session = requests.Session()
+response = session.post('https://www.mariettaga.gov/AgendaCenter', headers=headers, data=values)
 
 webpage = response.content
-
 soup = BeautifulSoup(webpage, 'html.parser')
 rows = soup.find(id="table7").tbody.find_all('tr')
 
