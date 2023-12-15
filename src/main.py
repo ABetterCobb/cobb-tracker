9d92b3c3ff8d83ca558d449e19ff8e20ff10ec7a
Guyliner <guyliner69@proton.me>
2023-12-15 14:09:10 -0500

    All years are now pulled for each agenda container

--- src/main.py
+++ src/main.py
@@ -7,6 +7,7 @@ from bs4 import BeautifulSoup
 
 URL_BASE = "https://www.mariettaga.gov"
 URL_AGENDAS = f"{URL_BASE}/AgendaCenter"
+URL_UPDATE_AGENDAS = f"{URL_BASE}/AgendaCenter/UpdateCategoryList"
 
 USER_AGENT = (
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:120.0) Gecko/20100101 Firefox/120.0"
@@ -54,45 +55,50 @@ def get_minutes_docs():
     agenda_containers = soup.find_all("div", class_="listing listingCollapse noHeader")
     for container in agenda_containers:
         year_list = get_years(container)
-        year_list[::]
-        sys.exit()
-        rows = container.find_all("tr", class_="catAgendaRow")
-        for row in rows:
-            meeting_title = row.find("a", {"aria-describedby": True}, target="_blank")
-            if meeting_title is None:
-                continue
-            meeting_name = clean_name(meeting_title.text.strip().title())
+        agenda_table_id = re.sub(r'[a-zA-Z]', '', container.find('table', summary="List of Agendas").get('id'))
+        for year in year_list:
+            payload = { 'year': year, 'catID': agenda_table_id }
+            agendas = session.post(URL_UPDATE_AGENDAS,\
+                    headers={"User-Agent": USER_AGENT}, \
+                    data=payload) 
+            new_doc = BeautifulSoup(agendas.text, 'html.parser')
+            rows = new_doc.find_all("tr", class_="catAgendaRow")
+            for row in rows:
+                meeting_title = row.find("a", {"aria-describedby": True}, target="_blank")
+                if meeting_title is None:
+                    continue
+                meeting_name = clean_name(meeting_title.text.strip().title())
 
-            date_header = row.find("td", class_=None)
-            print(date_header.strong.get_text() + " " + date_header.p.get_text().strip())
+                date_header = row.find("td", class_=None)
+                print(date_header.strong.get_text() + " " + date_header.p.get_text().strip())
 
-            minutes = row.find("td", class_="minutes")
-            minutes_link = minutes.find("a")
+                minutes = row.find("td", class_="minutes")
+                minutes_link = minutes.find("a")
 
-            if not minutes_link:
-                continue
-
-            minutes_url = f"{URL_BASE}{minutes_link.get('href')}"
-            minutes_name = minutes_url.split("/")[-1]
-
-            doc_id = minutes_name.split("-")[1]
-            year = minutes_name[5:9]
-            month = minutes_name[1:3]
-            day = minutes_name[3:5]
-
-            new_name = f"{year}_{month}_{day}_minutes_{doc_id}.pdf"
-
-            meeting_folder = MINUTES_FOLDER.joinpath(meeting_name)
-            meeting_folder.mkdir(exist_ok=True)
-
-            minutes_doc = session.get(minutes_url, headers={"User-Agent": USER_AGENT})
-            if minutes_doc.ok:
-                file_path = meeting_folder.joinpath(new_name)
-                if file_path.exists():
+                if not minutes_link:
                     continue
 
-                with open(file_path, "wb") as outfile:
-                    outfile.write(minutes_doc.content)
+                minutes_url = f"{URL_BASE}{minutes_link.get('href')}"
+                minutes_name = minutes_url.split("/")[-1]
+
+                doc_id = minutes_name.split("-")[1]
+                year = minutes_name[5:9]
+                month = minutes_name[1:3]
+                day = minutes_name[3:5]
+
+                new_name = f"{year}_{month}_{day}_minutes_{doc_id}.pdf"
+
+                meeting_folder = MINUTES_FOLDER.joinpath(meeting_name)
+                meeting_folder.mkdir(exist_ok=True)
+
+                minutes_doc = session.get(minutes_url, headers={"User-Agent": USER_AGENT})
+                if minutes_doc.ok:
+                    file_path = meeting_folder.joinpath(new_name)
+                    if file_path.exists():
+                        continue
+
+                    with open(file_path, "wb") as outfile:
+                        outfile.write(minutes_doc.content)
 
 
 if __name__ == "__main__":
