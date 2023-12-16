c218a3d9b25ecc63fd597a8098df66c8e1ac0a0c
Guyliner <guyliner69@proton.me>
2023-12-16 01:52:15 -0500

    Added municipalities.cobb import

--- src/main.py
+++ src/main.py
@@ -1,12 +1,15 @@
 import sys
 import argparse
 import municipalities.marietta
+import municipalities.cobb
 
 def choose_muni(municipality: str):
     muni = municipality.lower()
 
     if muni == "marietta":
         municipalities.marietta.get_minutes_docs()
+    if muni == "cobb":
+        municipalities.cobb.get_minutes_docs()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
