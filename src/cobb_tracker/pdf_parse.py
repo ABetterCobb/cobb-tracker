a468750e7ebb24e7a1dee7b51736baa22e07f6ae
Guyliner <guyliner69@proton.me>
2023-12-19 16:22:29 -0500

    Added municipality as a primary key for database entry

--- src/cobb_tracker/pdf_parse.py
+++ src/cobb_tracker/pdf_parse.py
@@ -27,8 +27,8 @@ def pdf_to_database(config: cobb_config):
     )
     if not DB["pages"].exists():
         DB["pages"].create(
-            {"body": str, "date": str, "page": int, "text": str},
-            pk=("body", "date", "page"),
+                { "municipality": str, "body": str, "date": str, "page": int, "text": str},
+            pk=( "municipality","body", "date", "page"),
         )
         DB["pages"].enable_fts(["text"], create_triggers=True)
     semaphore = Semaphore(len(os.sched_getaffinity(0)))
@@ -48,7 +48,8 @@ def write_to_database(config: cobb_config, minutes_file: str, DB: db.Database, s
         doc = fitz.open(file)
 
         rel_doc_path=file.replace(config.get_config('directories','minutes_dir'),'')
-        body = os.path.normpath(rel_doc_path).split(os.path.sep)[2]
+        municipality = os.path.normpath(rel_doc_path).split(os.path.sep)[1].replace('_',' ')
+        body = os.path.normpath(rel_doc_path).split(os.path.sep)[2].replace('_',' ')
         date = (os.path.split(Path(file))[1]).replace('-minutes.pdf','')
 
         print(f"Writing {rel_doc_path} contents to database")
@@ -60,6 +61,7 @@ def write_to_database(config: cobb_config, minutes_file: str, DB: db.Database, s
             page_text = pytesseract.image_to_string(Image.open(image_bytes))
             DB["pages"].insert(
                 {
+                    "municipality": municipality,
                     "body": body,
                     "date": date,
                     "page": page.number,
