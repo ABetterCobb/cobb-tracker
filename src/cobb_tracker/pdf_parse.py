0efa1f5a2a470c34475783c37e39e98c9dcaa10e
Guyliner <guyliner69@proton.me>
2023-12-21 08:45:24 -0500

    Exits if -p specified and no minutes files

--- src/cobb_tracker/pdf_parse.py
+++ src/cobb_tracker/pdf_parse.py
@@ -18,6 +18,7 @@ import math
 
 from cobb_tracker.municipalities import file_ops
 from cobb_tracker import file_ops
+import math
 from cobb_tracker.cobb_config import CobbConfig
 
 class DatabaseOps():
@@ -45,6 +46,7 @@ class DatabaseOps():
     def pdf_to_database(self):
         config = self.config
         DB = self.DB 
+        all_minutes_files = np.array(self.doc_ops.minutes_files())
         doc_ops = file_ops.FileList(minutes_dir=self.MINUTES_DIR)
 
         all_minutes_files = np.array(doc_ops.minutes_files())
