cf741e7789c08d849c394416d211077cb293040b
Guyliner <guyliner69@proton.me>
2023-12-26 19:10:43 -0500

    Comments

--- src/cobb_tracker/municipalities/smyrna.py
+++ src/cobb_tracker/municipalities/smyrna.py
@@ -27,6 +27,8 @@ def get_all_events(session: requests.Session) -> dict:
     took place it will also have either an Agenda or Minutes file
     """
     event_list = {}
+
+    #The records on Smyrna's primegov site only go back to 2013
     for year in range(2013,int(datetime.now().year)+1):
         raw_event_page = json.loads(session.get(f"{MEETINGS_URL}{year}",
                                                 headers={"User-Agent": USER_AGENT}).text)
@@ -43,16 +45,21 @@ def get_minutes_docs(config: CobbConfig):
                     r'(\b\d{1,2}\D{0,3})?\b(?:Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Oct(?:ober)?|(Nov|Dec)(?:ember)?)\D?(\d{1,2}\D?)?\D?((19[7-9]\d|20\d{2})|\d{2})\s?',
                     r'(^\s?)?(\s?$)?',
                     r'(\s-(.*))?( Meeting)?(\sNotice\sand\sAgenda)?']
+
     session = requests.Session()
     event_data = get_all_events(session)
+
     for year in event_data:
         for event in event_data[year]:
             event_date = datetime.strptime(
                     event["date"],
                     "%b %d, %Y"
                     ).strftime("%Y-%m-%d")
+
             for file in event["documentList"]:
                 if file["templateName"] == "Minutes":
+
+                    #The title might have new lines in it
                     event_title = event["title"].replace('\n','')
                     
                     for regex in smyrna_clean: 
