14edbaacd59f0cefaad6a1353195905b832b15ca
Guyliner <guyliner69@proton.me>
2023-12-28 08:56:42 -0500

    Saved

--- src/cobb_tracker/municipalities/powdersprings.py
+++ src/cobb_tracker/municipalities/powdersprings.py
@@ -4,6 +4,7 @@ import os
 import sys
 
 from autocorrect import Speller
+import dateutil.parser
 
 from cobb_tracker.municipalities import file_ops
 from cobb_tracker.cobb_config import CobbConfig
@@ -15,6 +16,9 @@ URL_BASE = "https://cityofpowdersprings.org/"
 LIST_OF_ARCHIVE_SECTIONS = f"{URL_BASE}Archive.aspx"
 BASE_FILE_URL = f"{URL_BASE}ArchiveCenter/ViewFile/Item/"
 FILE_PAGE = f"{LIST_OF_ARCHIVE_SECTIONS}?ADID="
+minutes_urls = {}
+
+#Pain
 get_year = re.compile(r'((\b(?:(Jan|JAN)(?:uary)?|(FEB|Feb)(?:ruary)?|(Mar|MAR)(?:ch)?|(Apr|APRIL)(?:il)?|(May|MAY)|(Jun|JUNE)(?:e)?|(JULY|Jul)(?:y)?|(AUG|Aug)(?:ust)?|(Sep|Sept|SEPT)(?:tember)?|(OCT|Oct)(?:ober)?|(Nov|NOV|Dec|DEC|DECEMBER)(?:ember)?)\s?(\.|,)?\s?(\d{1,2}\D?)?\D?\s?((19[7-9]\d|20\d{2})))|(\d{1,2}(\.|/|-|,)\s?\d{1,2}(\.|/|-|,)\s?\d{2,4})|([0-1][1-9][0-2][0-9][0-9][0-9])|((JAN|JANUARY)\s{1,2}\d{1,2},\s\d{2}))')
 
 USER_AGENT = (
@@ -25,9 +29,11 @@ def get_minutes_docs(config: CobbConfig):
     session = requests.Session()
     archive_groups = {} 
     response = session.get(LIST_OF_ARCHIVE_SECTIONS, headers={"User-Agent": USER_AGENT})
+
     if not response.ok:
         print("Request failed:", response.reason, response.status_code)
         return
+
     soup = BeautifulSoup(response.text, 'html.parser')
     archive_details = soup.find("table", summary="Archive Details")
 
@@ -39,50 +45,86 @@ def get_minutes_docs(config: CobbConfig):
                                           '',
                                           select_tag.get("id"))
                 archive_groups[group_id] = group
-
     for group in archive_groups.keys():
-        if archive_groups[group] != "Press Releases" and archive_groups[group] != "Town Hall Meetings and Retreats":
-            group_page = f"{LIST_OF_ARCHIVE_SECTIONS}?AMID={group}&Type=&ADID="
-            archive_page_response = session.get(group_page, headers={"User-Agent": USER_AGENT})
-            if not archive_page_response.ok:
-                print("Request failed:", archive_page_response.reason, archive_page_response.status_code)
-                return
-            archive_page = BeautifulSoup(archive_page_response.text, 'html.parser')
-            all_meetings = archive_page.find_all("span", class_="archive")
-            for span in all_meetings:
-                lines = [ line for line in span.text.split('\n') if line.strip()]
-                link = span.find("a")
-                name = span.find("span")
-                if link is not None and name is not None: 
-                    result = get_year.search(str(name.text))
-                    if result is not None:
-                        print(re.sub(r'([a-z]{1})(\d)',r'\1 \2',result.group(0)))
-                    else:
-                        corrected_title = (spell(name.text))
-                        result = get_year.search(str(corrected_title))
-                        if result is not None:
-                            print(result.group(0))
-                        else:
-                            #print(name.text)
-                            file_id = re.sub(r'[a-zA-Zw\.\?=]','',link.get('href'))
-                            singular_page = session.get(f"{FILE_PAGE}{file_id}", headers={"User-Agent": USER_AGENT})
-                            if not singular_page.ok:
-                                print("Request failed:", singular_page.reason, singular_page.status_code)
-                                return
-                            singular_page_text = BeautifulSoup(singular_page.text, 'html.parser')
-                            links = singular_page_text.find_all("a")
-                            for link in links:
-                                if link.get("href") is not None:
-                                    if "ArchiveCenter" in (link.get("href")):
-                                        link_text = link.text
-                                        lines = link_text.split('\n')
-                                        filtered_link_text = [line for line in lines if line.strip() != '']
-                                        date = get_year.search(filtered_link_text[0])
-                                        if date is not None:
-                                            print(re.sub(r'([a-z]{1})(\d)',r'\1 \2',date.group(0)))
-                                        #print(link.get("href"))
-                                        else:
-                                            print("borked link")
-                    #file_id = re.sub(r'[a-zA-Zw\.\?=]','',link.get('href'))
 
+    doc_ops = file_ops.FileOps(
+        file_urls=minutes_urls,
+        session=session,
+        user_agent=USER_AGENT,
+        config=config
+       )
+    doc_ops.write_minutes_doc()
+
+
+def parse_date(raw_date: str):
+    filtered_date = dateutil.parser.parse(re.sub(r'([a-z]{1})(\d)',r'\1 \2',raw_date))
+    return filtered_date
+    
+def get_meeting_info(group: dict): 
+
+    if archive_groups[group] != "Press Releases" and archive_groups[group] != "Town Hall Meetings and Retreats":
+        group_page = f"{LIST_OF_ARCHIVE_SECTIONS}?AMID={group}&Type=&ADID="
+        archive_page_response = session.get(group_page, headers={"User-Agent": USER_AGENT})
+
+        if not archive_page_response.ok:
+            print("Request failed:", archive_page_response.reason, archive_page_response.status_code)
+            return
+
+        archive_page = BeautifulSoup(archive_page_response.text, 'html.parser')
+        '''
+        On this "archive_page" , there is a span tag for every link.
+        The titles of said links are the only date information
+        that is available for these meetings without actually looking
+        in the file. The dates are in many different formats, and need
+        to be parsed. 
+
+        If a date cannot be parsed, due to a typo or 
+        a date just not existing, we check the actual page for the
+        individual PDF and see if that has date information. 
+        If that doesn't work the file has no date.
+        '''
+
+        all_meetings = archive_page.find_all("span", class_="archive")
+
+        for span in all_meetings:
+            lines = [ line for line in span.text.split('\n') if line.strip()]
+            link = span.find("a")
+            name = span.find("span")
+
+            if link is not None and name is not None: 
+                file_id = re.sub(r'[a-zA-Zw\.\?=]','',link.get('href'))
+                result = get_year.search(str(name.text))
+
+                if result is not None:
+                    print(parse_date(result.group(0))
+
+                else:
+                    corrected_title = (spell(name.text))
+                    result = get_year.search(str(corrected_title))
+
+                    if result is not None:
+                        print(parse_date(result.group(0)))
+
+                    else:
+                        singular_page = session.get(f"{FILE_PAGE}{file_id}", headers={"User-Agent": USER_AGENT})
+
+                        if not singular_page.ok:
+                            print("Request failed:", singular_page.reason, singular_page.status_code)
+                            return
+                        singular_page_text = BeautifulSoup(singular_page.text, 'html.parser')
+                        links = singular_page_text.find_all("a")
+
+                        for link in links:
+                            if link.get("href") is not None:
+                                if "ArchiveCenter" in (link.get("href")):
+                                    link_text = link.text
+                                    lines = link_text.split('\n')
+                                    filtered_link_text = [line for line in lines if line.strip() != '']
+                                    date = get_year.search(filtered_link_text[0])
+
+                                    if date is not None:
+                                        print(parse_date(date.group(0)))
+
+                                    else:
+                                        print("date unknown")
 
