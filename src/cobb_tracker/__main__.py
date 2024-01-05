2595462ee206a2c5c0a91c0f25382187b45ebb72
GuyLiner <guyliner69@proton.me>
2024-01-04 23:12:08 -0500

    All cities can now be pulled at once

--- src/cobb_tracker/__main__.py
+++ src/cobb_tracker/__main__.py
@@ -18,31 +18,29 @@ def choose_muni(municipality: str, config: CobbConfig):
         municipality (str): Where are you?    
     """
     muni = municipality.lower()
-    if muni == "marietta":
+    if muni == "marietta" or config.args.pull_all_cities:
         marietta.get_minutes_docs(config=config)
 
-    elif muni == "cobb":
+    if muni == "cobb" or config.args.pull_all_cities:
         cobb_civic = civicplus.CivicPlus(base_url="https://cobbcoga.api.civicclerk.com/v1", muni="Cobb")
         cobb_civic.get_minutes_docs(config=config)
 
-    elif muni == "austell":
+    if muni == "austell" or config.args.pull_all_cities:
         austell.get_minutes_docs(config=config)
 
-    elif muni == "acworth":
+    if muni == "acworth" or config.args.pull_all_cities:
         acworth.get_minutes_docs(config=config)
     
-    elif muni == "powdersprings":
+    if muni == "powdersprings" or config.args.pull_all_cities:
         powdersprings.get_minutes_docs(config=config) 
 
-    elif muni == "kennesaw":
+    if muni == "kennesaw" or config.args.pull_all_cities:
         kennesaw_civic = civicplus.CivicPlus(base_url="https://kennesawga.api.civicclerk.com/v1", muni="Kennesaw")
         kennesaw_civic.get_minutes_docs(config=config)
         novusagenda.get_minutes_docs(config=config)
 
-    elif muni == "smyrna":
+    if muni == "smyrna" or config.args.pull_all_cities:
         smyrna.get_minutes_docs(config=config)
-    else:
-        print("Municipality not recognized")
 
 def main():
     if os.name != "posix":
@@ -60,18 +58,30 @@ def main():
             action="store_true",
             )
     parser.add_argument(
+            "-a",
+            "--pull-all-cities",
+            action="store_true",
+            )
+    parser.add_argument(
             "-f",
             "--force",
             action="store_true",
             )
     args = parser.parse_args()
-    if args.push_to_database is False and args.municipality is None:
+    if args.push_to_database is False and args.municipality is None and args.pull_all_cities is None:
         print("error: You must specify -m/--municipality if -p/--push-to-database is not selected")
         sys.exit()
+
+    if args.pull_all_cities is True and args.municipality is not None:
+        print("error: -m/--municipality and -a/--pull-all-cities are mutually exclusive")
+        sys.exit()
+
 
     config = CobbConfig(flags=args)
     if args.municipality is not None:
         choose_muni(args.municipality, config)
+    if args.municipality is None:
+        choose_muni("all", config)
 
     if args.push_to_database:
         pdf_to_db = DatabaseOps(config)
