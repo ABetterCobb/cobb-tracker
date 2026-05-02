0368de83c938b9b4e656250255af5048ea2e2074
Sam Foster <samfoster@disroot.org>
2026-05-01 22:38:00 -0400

    powdersprings.py: URL has been changed to powderspringsga.gov

--- src/cobb_tracker/municipalities/powdersprings.py
+++ src/cobb_tracker/municipalities/powdersprings.py
@@ -24,7 +24,7 @@ from cobb_tracker.cobb_config import CobbConfig
 from cobb_tracker.string_ops import parse_date
 
 
-URL_BASE = "https://cityofpowdersprings.org/"
+URL_BASE = "https://www.powderspringsga.gov/"
 LIST_OF_ARCHIVE_SECTIONS = f"{URL_BASE}Archive.aspx"
 BASE_FILE_URL = f"{URL_BASE}ArchiveCenter/ViewFile/Item/"
 FILE_PAGE = f"{LIST_OF_ARCHIVE_SECTIONS}?ADID="
