49aec04bde45d3683a115fa1e0c3d0e7121e72ae
Sam Foster <samfoster@disroot.org>
2026-08-04 13:35:00 -0400

    pdf_parse: Fixed bug where documents were uploaded in the data block for paperless instead of the files block

--- src/cobb_tracker/pdf_parse.py
+++ src/cobb_tracker/pdf_parse.py
@@ -50,6 +50,16 @@ class PaperlessOps:
         response = requests.get(doc_type_query, headers=self.HEADERS).json()
 
         return response["results"][0]["id"]
+    def doc_exists(self, checksum: str):
+        docs_url = f"{self.PAPERLESS_URL}/api/documents/"
+        doc_query = f"{docs_url}?checksum__iexact={checksum}"
+        response = requests.get(doc_query, headers=self.HEADERS).json()
+        
+        if response["count"] == 0:
+            return False
+        else:
+            return True
+
     def get_tag(self, tag: str):
         tag_url = f"{self.PAPERLESS_URL}/api/tags/"
         tag_query = f"{tag_url}?name__iexact={tag}"
@@ -95,77 +105,81 @@ class PaperlessOps:
             The PDFs are sent to paperless ngx. Provide the URL and the Token
         """
         with self.SEMAPHORE:
-            post_document=f"{self.PAPERLESS_URL}/api/documents/post_document/"
-            correspondents=f"{self.PAPERLESS_URL}/api/correspondents/"
-            doc_types=f"{self.PAPERLESS_URL}/api/document_types/"
-            file = str(minutes_file)
+            checksum = str(self.doc_ops.get_checksum(Path(minutes_file)))
+            if not self.doc_exists(checksum):
+                post_document=f"{self.PAPERLESS_URL}/api/documents/post_document/"
+                correspondents=f"{self.PAPERLESS_URL}/api/correspondents/"
+                doc_types=f"{self.PAPERLESS_URL}/api/document_types/"
+                file = str(minutes_file)
 
-            rel_doc_path = file.replace(
-                self.config.get_config("directories", "minutes_dir"), ""
-            )
-            municipality = (
-                os.path.normpath(rel_doc_path)
-                .split(os.path.sep)[1]
-                .replace("_", " ")
-            )
+                rel_doc_path = file.replace(
+                    self.config.get_config("directories", "minutes_dir"), ""
+                )
+                municipality = (
+                    os.path.normpath(rel_doc_path)
+                    .split(os.path.sep)[1]
+                    .replace("_", " ")
+                )
 
-            #Set correspondent to municipality
-            muni_query=f"{correspondents}?name__iexact={municipality}"
-            muni_correspondent = requests.get(muni_query, headers=self.HEADERS).json()
+                #Set correspondent to municipality
+                muni_query=f"{correspondents}?name__iexact={municipality}"
+                muni_correspondent = requests.get(muni_query, headers=self.HEADERS).json()
 
-            if muni_correspondent["count"] == 0:
-                data = {
-                    "name": municipality,
-                    "owner": 1
-                }
-                response = requests.post(correspondents, headers=self.HEADERS, data=data)
+                if muni_correspondent["count"] == 0:
+                    data = {
+                        "name": municipality,
+                        "owner": 1
+                    }
+                    response = requests.post(correspondents, headers=self.HEADERS, data=data)
 
-            muni_correspondent = requests.get(muni_query, headers=self.HEADERS).json()
-            muni_corr_id = muni_correspondent["results"][0]["id"]
+                muni_correspondent = requests.get(muni_query, headers=self.HEADERS).json()
+                muni_corr_id = muni_correspondent["results"][0]["id"]
 
-            #Get Committee/Body
-            body = (
-                os.path.normpath(rel_doc_path)
-                .split(os.path.sep)[2]
-                .replace("_", " ")
-            )
-            if self.get_tag(body) is None:
-                self.set_tag(body)
-             
-            #Set document type
-            if "minutes" in os.path.split(Path(file))[1]:
-                doc_type = "Minutes"
+                #Get Committee/Body
+                body = (
+                    os.path.normpath(rel_doc_path)
+                    .split(os.path.sep)[2]
+                    .replace("_", " ")
+                )
+                if self.get_tag(body) is None:
+                    self.set_tag(body)
+                 
+                #Set document type
+                if "minutes" in os.path.split(Path(file))[1]:
+                    doc_type = "Minutes"
 
-            doc_type_query = f"{doc_types}?name__iexact={doc_type}"
-            document_type = requests.get(doc_type_query, headers=self.HEADERS).json()
-            if document_type["count"] == 0:
-                data = {
-                    "name": doc_type,
-                    "owner": 1
-                }
-                response = requests.post(doc_types, headers=self.HEADERS, data=data)
+                doc_type_query = f"{doc_types}?name__iexact={doc_type}"
+                document_type = requests.get(doc_type_query, headers=self.HEADERS).json()
+                if document_type["count"] == 0:
+                    data = {
+                        "name": doc_type,
+                        "owner": 1
+                    }
+                    response = requests.post(doc_types, headers=self.HEADERS, data=data)
 
 
-            #Post file
-            match = re.match(r"(\d{4}-\d{2}-\d{2})", (os.path.split(Path(file))[1]))
+                #Post file
+                match = re.match(r"(\d{4}-\d{2}-\d{2})", (os.path.split(Path(file))[1]))
 
-            date = match.group(1)
+                date = match.group(1)
+                
+                with open(minutes_file, "rb") as f:
+                    files = {
+                        "document": (os.path.basename(minutes_file), f, "application/pdf"),
+                    }
 
-            files = {
-                "document": (minutes_file, open(minutes_file, 'rb'), "application/pdf"),
-            }
+                    data = {
+                        "created": date,
+                        #"document": open(minutes_file,'rb'),
+                        "document_type": self.get_document_type(doc_type), 
+                        "title": f"{body} minutes",
+                        "tags": [self.get_tag(body)],
+                        "correspondent": muni_corr_id,
+                    }
 
-            data = {
-                "created": date,
-                "document": open(minutes_file,'rb'),
-                "document_type": self.get_document_type(doc_type), 
-                "title": f"{body} minutes",
-                "tags": [self.get_tag(body)],
-                "from_webui": False,
-                "correspondent": muni_corr_id,
-            }
+                    post_document=f"{self.PAPERLESS_URL}/api/documents/post_document/"
+                    time.sleep(1.5)
+                    response = requests.post(post_document, headers=self.HEADERS, files=files, data=data)
 
-            post_document=f"{self.PAPERLESS_URL}/api/documents/post_document/"
-            time.sleep(1.5)
-            response = requests.post(post_document, headers=self.HEADERS, files=files, data=data)
-            print(response.content, date)
+                    if not response.ok:
+                        print(response.content, date, minutes_file, data)
